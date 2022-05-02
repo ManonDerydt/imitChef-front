64630f7d42b558984526cc0d06fb79ea8527877b
manon <derydtmanon@gmail.com>
2022-05-02 21:51:53 +0200

    Création d'un service avec deux méthodes pour enregistrer et récupérer le token d'identification dans le local storage ionic

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class StorageService {
+    private _storage: Storage | null = null;
+
+    constructor(private storage: Storage) {
+        this.init();
+    }
+
+    async init() {
+        const storage = await this.storage.create();
+        this._storage = storage;
+        console.log("storage", this._storage)
+    }
+
+    public async set(key: string, value: any) {
+        await this._storage?.set(key, value);
+    }
+
+    public async get(key: string) {
+        return await this._storage.get(key);
+    }
+}
