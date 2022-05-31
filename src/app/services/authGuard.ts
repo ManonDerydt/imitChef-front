8966632f840cbc585e75ec237a00617508917b91
marc <bernini.marc@gmail.com>
2022-05-31 20:54:48 +0200

    Création d'une guard pour protéger les routes accessibles uniquement si l'utilisateur est connecté

---
+++ src/app/services/authGuard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { StorageService } from './storage.service';
+
+@Injectable()
+export class AuthGuardService implements CanActivate {
+  constructor(public storageService: StorageService, public router: Router) {}
+  async canActivate(): Promise<boolean> {
+      const user = await this.storageService.get("user");
+      if (user) {
+          return true;
+      } else {
+          this.router.navigate(["home/sign_in"]);
+          return false;
+      }
+  }
+}
