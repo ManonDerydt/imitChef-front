7f387b1d5d35de8f968427d322160ec3ad0e1515
manon <derydtmanon@gmail.com>
2022-05-31 19:26:28 +0200

    Récupération du User et de ces reciepes

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {ApiService} from './api.service';
+import {Observable} from "rxjs";
 
 
 @Injectable({
@@ -17,4 +18,17 @@ export class UserService {
     loginUser(password: string, email: string) {
         return this.api.post("/users/login", {password, email});
     }
+
+    // getUser(){
+    //     return this.api.get(`/users/account`);
+    // }
+
+    getReciepesByUser() {
+        return this.api.get(`/users/reciepes`);
+    }
+
+    getUser(){
+        return this.api.get(`/users`);
+    }
+
 }
