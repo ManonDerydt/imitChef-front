a6d23861db7391266a6e95d2e40d31366be13946
manon <derydtmanon@gmail.com>
2022-09-15 16:06:28 +0200

    Ajouts des offres

--- src/app/models/deal.ts
+++ src/app/models/deal.ts
@@ -1,9 +1,17 @@
-import { Restaurant } from "./restaurant";
+import {Order} from "./order";
 
 export class Deal {
     id: number;
     deadline: string;
     max_people: string;
     price: string;
-    Restaurants: Array<Restaurant>
+    image: string;
+    adress: string;
+    city: string;
+    number: string;
+    email: string;
+    chef: string;
+    restaurant: string;
+    menu: string;
+    Orders: Array<Order>;
 }
